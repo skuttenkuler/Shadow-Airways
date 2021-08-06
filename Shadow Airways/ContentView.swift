76edb7154041da2584485a846314fa3ccb5902fb
skuttenkuler <sam.kuttenk@gmail.com>
2021-08-05 23:05:04 -0500

    Initial Commit

---
+++ Shadow Airways/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shadow Airways
+//
+//  Created by Sam Kuttenkuler on 8/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shadow Airways/Shadow_AirwaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shadow_AirwaysApp.swift
+//  Shadow Airways
+//
+//  Created by Sam Kuttenkuler on 8/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Shadow_AirwaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
