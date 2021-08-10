9763db122b79531b1b773284eb3025061f667c00
skuttenkuler <sam.kuttenk@gmail.com>
2021-08-09 20:36:44 -0500

    added date time picker, navigation link to final view

--- Shadow Airways/BookTicketView.swift
+++ Shadow Airways/BookTicketView.swift
@@ -7,29 +7,41 @@
 
 import SwiftUI
 
-class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let airData = DataLoader()
-        print(airData)
-    }
-}
 struct BookTicketView: View {
+    
+    
     var body: some View {
+        VStack{
         Text("BOOK TICKET")
-        
+        BookTicketContent()
     }
 }
 
 struct BookTicketContent: View {
+    //variables
+    @State private var departure = Date()
+    @State private var returnDate = Date()
+    
     //view block
     var body: some View {
-        Text("hello")
         //Origin
         //Destination
         //Departure Date
+        DatePicker("Departure", selection: $departure, in: Date()...)
         //Return Date
+        DatePicker("Return", selection: $returnDate, in: Date()...)
         //logic to get data and pass to next view
         //Navigation Link to ticket
+        Spacer()
+        NavigationLink(destination:TicketView(), label: {
+            Text("BOOK FLIGHT")
+                .font(.system(size:60))
+                .foregroundColor(Color.white)
+                .background(Color.blue)
+                .cornerRadius(20)
+                
+            })
+        }
     }
+    
 }
