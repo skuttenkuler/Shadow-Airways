17d996fc5c415bc236554ea6816a95ca8a31160d
skuttenkuler <sam.kuttenk@gmail.com>
2021-08-21 19:58:28 -0500

    react navigation working, add AppRegistry, stack set up

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import Home from './Screens/Home'
+import BookTicket from './Screens/BookTicket'
+import Ticket from './Screens/Ticket'
+
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#621FF7',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle :{
+            fontWeight: 'bold',
+          },
+        }}
+      >
+      <Stack.Screen 
+        name="Home" 
+        component={Home} 
+        options={{ title: 'Home' }}
+      />
+      <Stack.Screen 
+        name="Book Ticket" 
+        component={BookTicket} 
+        options={{ title: 'BookTicket' }}
+      />
+      <Stack.Screen 
+       name="Ticket" 
+       component={Ticket} 
+       options={{ title: 'Ticket' }}
+      />
+    </Stack.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App(){
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  )
+}
