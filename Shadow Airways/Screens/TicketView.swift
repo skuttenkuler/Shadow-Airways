d5dbb4f1f13b7400bc8f724d87f7cac26fa00c56
skuttenkuler <sam.kuttenk@gmail.com>
2021-09-02 09:56:45 -0500

    adjusted frame of ticket, now snapshot captures full screen, adjusted text views

--- Shadow Airways/Screens/TicketView.swift
+++ Shadow Airways/Screens/TicketView.swift
@@ -11,19 +11,19 @@ struct TicketView: View {
     var body: some View {
         ZStack{
             VStack{
-            Text("FROM:").offset(x: 0,y: -180).font(.system(size:25).weight(.heavy))
-            Text("TO:").offset(x: 0,y: -150).font(.system(size:25).weight(.heavy))
-            Text("Departing:").offset(x: 10,y: 160).font(.system(size:30).weight(.heavy))
-            Text("Date:").offset(x: -130,y: 260).font(.system(size:25).weight(.heavy))
+            Text("FROM:").offset(x: -30,y: -95).font(.system(size:25).weight(.heavy))
+            Text("TO:").offset(x: -50,y: -80).font(.system(size:25).weight(.heavy))
+            Text("Departing:").offset(x: 35,y: 138).font(.system(size:25).weight(.heavy))
+            Text("Date:").offset(x: -95,y: 190).font(.system(size:25).weight(.heavy))
         Button("Book"){
             let image = snapshot()
             UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
-        }
+        }.offset(x: 0 , y: 250).frame(maxWidth:.infinity)
     }.background(
         Image("Ticket2")
             .resizable()
             .edgesIgnoringSafeArea(.all)
-            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            .frame(width:300, height: 650)
     )}
 }
 
